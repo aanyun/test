487847a6fbbbf8ab1d3bca88199808d1a5b64331
aanyun <anyunww@gmail.com>
2014-06-28 00:24:00 +0000

    github

--- github.php
+++ github.php
@@ -1,11 +1,6 @@
 <?php
-// Check for safe mode
-if( ini_get('safe_mode') ){
-    echo "safe_mode";
-}else{
-    echo "hello";
-}
-echo shell_exec('/usr/bin/git pull origin master 2>&1');
+
+echo shell_exec('git pull origin master');
 
 
 ?>
