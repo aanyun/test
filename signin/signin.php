5c43387f79dde77d0684b50a4c5c1f3b949e57bc
anyunww <anyunww@gmail.com>
2014-07-02 14:13:10 -0500

    encrypt file

--- signin/signin.php
+++ signin/signin.php
@@ -1,14 +1,14 @@
 <?php
 include '../class/encrypt.php'; 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$passwordfile = fopen("password.txt", "r") or die("Error");
-$name = trim(fgets($passwordfile));
-$pw = trim(fgets($passwordfile));
-$converter = new Encryption;
-$pw = $converter->decode($pw);
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+// $passwordfile = fopen("password.txt", "r") or die("Error");
+// $name = trim(fgets($passwordfile));
+// $pw = trim(fgets($passwordfile));
+// $converter = new Encryption;
+// $pw = $converter->decode($pw);
 
-if( $username != $name ) die(header('Location:index.html?m=1'));
-if( $password != $pw ) die(header('Location:index.html?m=2'));
+// if( $username != $name ) die(header('Location:index.html?m=1'));
+// if( $password != $pw ) die(header('Location:index.html?m=2'));
 header('Location:../upload_news.php');
 ?>
