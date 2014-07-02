b0bbf1bd8cfae0226247ef698e26c2518a36d1d5
anyunww <anyunww@gmail.com>
2014-07-02 14:16:03 -0500

    login

--- signin/signin.php
+++ signin/signin.php
@@ -3,10 +3,10 @@ include '../class/encrypt.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 $passwordfile = fopen("password.txt", "r") or die("Error");
-// $name = trim(fgets($passwordfile));
-// $pw = trim(fgets($passwordfile));
-// $converter = new Encryption;
-// $pw = $converter->decode($pw);
+$name = trim(fgets($passwordfile));
+$pw = trim(fgets($passwordfile));
+$converter = new Encryption;
+$pw = $converter->decode($pw);
 
 // if( $username != $name ) die(header('Location:index.html?m=1'));
 // if( $password != $pw ) die(header('Location:index.html?m=2'));
