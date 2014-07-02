f548aafe9b388fad39d70f226736096b9e8e8c63
anyunww <anyunww@gmail.com>
2014-07-02 14:23:31 -0500

    login

--- signin/signin.php
+++ signin/signin.php
@@ -6,7 +6,7 @@ $passwordfile = fopen("password.txt", "r") or die("Error");
 $name = trim(fgets($passwordfile));
 $pw = trim(fgets($passwordfile));
 $converter = new Encryption;
-// $pw = $converter->decode($pw);
+$pw = $converter->decode($pw);
 
 if( $username != $name ) die(header('Location:index.html?m=1'));
 if( $password != $pw ) die(header('Location:index.html?m=2'));
