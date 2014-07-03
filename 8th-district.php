742a42b3bc6c596439fd3767ac62b12ffeca166c
doririvera <d-rivera@ignitorlabs.com>
2014-07-02 20:02:05 -0500

    8th district addition

---
+++ 8th-district.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kaifesh for Congress Official Website | Get Involved</title>
+
+    <!-- Bootstrap core CSS .. -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/modern-business.css" rel="stylesheet">
+	<link href="css/font.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	
+	
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="container">
+    	<?php include "view/header.php";?>
+	</div> <!-- / .container -->
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+			
+			   <h1 class="page-header"><strong>8th District</strong><br />
+                    <small>The map below shows the 8th Congressional District boundaries.</h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a>
+                    </li>
+                    <li class="active">8th District</li>
+                </ol>
+			  
+            </div>
+			
+			</div>
+			
+			
+			            <div class="col-lg-12">
+
+	            <div class="row">
+
+           	    <p>*Portions of these towns or villages are outside of the 8th District. To be sure which district you reside in, <a href="http://www.elections.il.gov/districtlocator/districtofficialsearchbyaddress.aspx" target="_blank">click here.</a></small></p>
+				<p>Are you unsure of where to vote? To request information about voter registration and polling place locations, <a href="http://www.elections.il.gov/VotingInformation/RegistrationLookup.aspx" target="_blank">click here.</a>
+				
+				<div class="row">
+				
+				<div class="col-lg-6">
+				<iframe width="425" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+src="http://www.govtrack.us/congress/members/embed/mapframe?state=il&district=8&bounds=-89.199,42.525,-87.06,41.463"></iframe>
+				</div>
+				
+				
+				<div class="col-lg-6">
+				test
+				</div>
+				
+				</div>
+  
+		   
+		   
+				</div>
+				
+				
+					</div>
+
+    </div>
+    <!-- /.container -->
+
+	
+	
+	
+	    <div class="section">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-7 col-md-7 subscribe">
+                    <h2><strong>Stay Updated</strong><span style="font-size: 17px; color: #494949;"> &ndash; Subscribe for the latest campaign updates!</span></h3>
+                    
+					
+					<form class="form-inline" role="form">
+					<div class="row" style="margin:0;">
+					  <div class="form-group adjust1">
+						<label class="sr-only">First Name</label>
+						<input name="firstName" class="form-control" placeholder="First Name">
+					  </div>
+					  <div class="form-group">
+						<label class="sr-only">Last Name</label>
+						<input name="lastName" class="form-control" placeholder="Last Name">
+					  </div>
+
+						  <div class="form-group adjust1" style="padding-top: 15px;">
+							<label class="sr-only">Email Address</label>
+							<input name="emailAddress" class="form-control" placeholder="Email Address">
+						  </div>
+						  <div class="form-group" style="padding-top: 15px;">
+							<label class="sr-only">Zip Code</label>
+							<input name="zipCode" class="form-control-sm" placeholder="Zip Code">
+						  </div>
+						  <span style="position: relative; top: 8px;">
+						  <button type="submit" class="btn btn-danger" style="margin-left: -8px;">Submit</button>
+						</span>
+					  </div> <!-- /.row --> 
+					</form>
+					
+					
+                </div>
+                <div class="col-lg-4 col-md-4 connect adjust2">
+					<h2><strong>Stay Connected</strong><span style="font-size: 17px; color: #494949;"> with Larry Kaifesh!</span></h3>
+					<hr class="divideConnect"> </hr>
+					
+					<a href="https://www.facebook.com/Kaifeshforcongress" target="_blank">
+						<img src="img/fb_icon.png" class="connect_icon">
+					</a>
+					<a href="https://twitter.com/LarryKaifesh" target="_blank">
+						<img src="img/twitter_icon.png" class="connect_icon">
+					</a>
+					<a href="https://www.youtube.com/user/kaifeshforcongress" target="_blank">
+						<img src="img/youtube_icon.png" class="connect_icon">
+					</a>
+					<a href="https://www.linkedin.com/in/kaifeshforcongress" target="_blank">
+						<img src="img/linkedin_icon.png" class="connect_icon">
+					</a>					
+					<a href="https://plus.google.com/110076519787570184443/posts" target="_blank">
+						<img src="img/gplus_icon.png" class="connect_icon">
+					</a>
+					<a href="http://www.pinterest.com/larrykaifesh/" target="_blank">
+						<img src="img/pin_icon.png" class="connect_icon">
+					</a>
+					<a href="mailto:larry@kaifeshforcongress.com">
+						<img src="img/email_icon.png" class="position: relative; top: -3px;">
+					</a>
+
+						<div style="margin-top: 15px;">
+					<button type="button" class="btn btn-lg btn-primary" style="margin-right: 11px;">Contribute</button>
+					<button type="button" class="btn btn-lg btn-danger">Get Involved</button>
+						</div>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.section -->
+	
+	
+        <footer>
+    <div class="section-colored">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-4">
+				<div class="paidforby">Paid for by Kaifesh for Congress</div>
+				<p class="subfooter">636 E. Irving Park Road<br />
+					Roselle, IL 60172<br />
+					Office: 847-380-6915<br />
+					After hours: 224-800-8065</p>
+                </div>
+				<div class="col-lg-4 subfooter-center">
+                    <p>Copyright &copy; Kaifesh for Congress 2014.</p>
+					<img src="img/kaifesh_logo.png" class="img2" alt="Kaifesh for Congress">
+				</div>
+				<div class="col-lg-4 subfooter-right">
+                    <p><a href="#">Privacy Policy</a> | <a href="#">Contact</a></p>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+	
+    <!-- /.section-colored -->
+	    <div class="section-colored2 text-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="desclaimer">Larry Kaifesh is a Colonel in the United States Marine Corps Reserve.
+					<br />Use of his military rank, job titles, and photographs in uniform does not imply endorsement by the Marine Corps, Department of the Navy or the Department of Defense. </p>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.section-colored -->
+	</footer>
+	
+</div>
+<!-- /.wrapper -->
+
+
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/modern-business.js"></script>
+	<script src="js/jquery-1.9.1.js"></script>
+	<script src="js/main.js"></script>	
+
+</body>
+
+</html>
