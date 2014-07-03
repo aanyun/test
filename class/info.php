980aa4b410484c174d230fc88fdb71d1ab7990f3
anyunww <anyunww@gmail.com>
2014-07-03 12:01:08 -0500

    login

--- class/info.php
+++ class/info.php
@@ -1,23 +1,8 @@
 <?php
 
 
-function return_bytes($val) {
-    $val = trim($val);
-    $last = strtolower($val[strlen($val)-1]);
-    switch($last) {
-        // The 'G' modifier is available since PHP 5.1.0
-        case 'g':
-            $val *= 1024;
-        case 'm':
-            $val *= 1024;
-        case 'k':
-            $val *= 1024;
-    }
 
-    return $val;
-}
-
-echo return_bytes(ini_get('post_max_size'));
+echo ini_get('post_max_size');
 
 
 ?>
