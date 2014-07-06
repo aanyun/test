c39e4949edf9d9a840e9a4037e580b4415e5acfd
anyunww <anyunww@gmail.com>
2014-07-06 13:58:31 -0500

    a

--- media-gallery.php
+++ media-gallery.php
@@ -78,7 +78,7 @@
 	                	}
                 ?>	
                 	<a href='<?php echo $videolink?>' rel="prettyPhoto[mixed]" alt="<?php echo $new['title']?>" title="<?php echo $new['desc']?>">
-	                <iframe width="200" height="150"
+	                <iframe width="100%" height="100%"
 					src='<?php echo $new["url"]?>'>
 					</iframe>
 					</a>
