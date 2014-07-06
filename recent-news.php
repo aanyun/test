5e2ef785a5fd042412bcf2ca206dc2350a7cd896
anyunww <anyunww@gmail.com>
2014-07-06 15:03:56 -0500

    a

--- recent-news.php
+++ recent-news.php
@@ -113,7 +113,7 @@
                                 <?php 
                                 
                                 for($i=0;$i<12;$i++){
-                                	echo "<li><a href='".date('Y-m',strtotime('-'.$i.' month'))."'>".date('M Y',strtotime("-".$i." month"))."</a></li>";
+                                	echo "<li><a href='".date('Y-m',strtotime('-'.$i.' month'))."'>".date('F Y',strtotime("-".$i." month"))."</a></li>";
                             	}
                                 ?>
                             </ul>
