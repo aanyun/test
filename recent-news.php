a4272aecb3e451e090b8a48bfb26b9b9b98f3044
anyunww <anyunww@gmail.com>
2014-07-06 15:32:42 -0500

    a

--- recent-news.php
+++ recent-news.php
@@ -29,8 +29,8 @@ ini_set("display_errors", 1);
 		include "class/MysqliDb.php";
 		$db = new Mysqlidb();
 		$current_page = (isset($_GET['page'])&& $_GET['page']!='')? $_GET['page']-1:0;
-		$date = (isset($_GET['date'])&& $_GET['date']!='')? $_GET['date']:'';
-		$query = "select * from news,publishers where publishers.id=idPublisher and date=".$date." order by date desc,news.id desc limit 5 offset ".$current_page*5;
+		$date = (isset($_GET['date'])&& $_GET['date']!='')? $_GET['date']:"";
+		$query = "select * from news,publishers where publishers.id=idPublisher and date='".$date."' order by date desc,news.id desc limit 5 offset ".$current_page*5;
 		//echo $query;
 		$news = $db->rawQuery($query);
 
