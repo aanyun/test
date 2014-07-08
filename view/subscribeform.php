f18f974f67d7cd033bef4cae9bbd7df2b8c3cf3c
anyunww <anyunww@gmail.com>
2014-07-07 20:17:19 -0500

    move subscribe to view

---
+++ view/subscribeform.php
@@ -0,0 +1,27 @@
+<h2><strong>Stay Updated</strong><span style="font-size: 17px; color: #494949;"> &ndash; Subscribe for the latest campaign updates!</span></h3>
+
+
+<form class="form-inline" role="form">
+<div class="row" style="margin:0;">
+  <div class="form-group adjust1">
+	<label class="sr-only">First Name</label>
+	<input name="firstName" class="form-control" placeholder="First Name">
+  </div>
+  <div class="form-group">
+	<label class="sr-only">Last Name</label>
+	<input name="lastName" class="form-control" placeholder="Last Name">
+  </div>
+
+	  <div class="form-group adjust1" style="padding-top: 15px;">
+		<label class="sr-only">Email Address</label>
+		<input name="emailAddress" class="form-control" placeholder="Email Address">
+	  </div>
+	  <div class="form-group" style="padding-top: 15px;">
+		<label class="sr-only">Zip Code</label>
+		<input name="zipCode" class="form-control-sm" placeholder="Zip Code">
+	  </div>
+	  <span style="position: relative; top: 8px;">
+	  <button type="submit" class="btn btn-danger" style="margin-left: -8px;">Submit</button>
+	</span>
+  </div> <!-- /.row --> 
+</form>
