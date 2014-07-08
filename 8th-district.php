c75de485369ad79013fbc62c21f64d2d4438b3da
anyunww <anyunww@gmail.com>
2014-07-07 21:24:00 -0500

    move subscribe to view

--- 8th-district.php
+++ 8th-district.php
@@ -130,35 +130,7 @@ src="http://www.govtrack.us/congress/members/embed/mapframe?state=il&district=8&
 
             <div class="row">
                 <div class="col-lg-8 col-md-6 col-sm-6 subscribe">
-                    <h2><strong>Stay Updated</strong><span style="font-size: 17px; color: #494949;"> &ndash; Subscribe for the latest campaign updates!</span></h3>
-                    
-					
-					<form class="form-inline" role="form">
-					<div class="row" style="margin:0;">
-					  <div class="form-group adjust1">
-						<label class="sr-only">First Name</label>
-						<input name="firstName" class="form-control" placeholder="First Name">
-					  </div>
-					  <div class="form-group fix">
-						<label class="sr-only">Last Name</label>
-						<input name="lastName" class="form-control" placeholder="Last Name">
-					  </div>
-
-						  <div class="form-group adjust1" style="padding-top: 15px;">
-							<label class="sr-only">Email Address</label>
-							<input name="emailAddress" class="form-control" placeholder="Email Address">
-						  </div>
-						  <div class="form-group" style="padding-top: 15px;">
-							<label class="sr-only">Zip Code</label>
-							<input name="zipCode" class="form-control-sm" placeholder="Zip Code">
-						  </div>
-						  <span class="subscribeBtn">
-						  <button type="submit" class="btn btn-danger" style="margin-left: -8px;">Submit</button>
-						</span>
-					  </div> <!-- /.row --> 
-					</form>
-					
-					
+                	<?php include "view/subscribeform.php";?>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 connect adjust2">
 					<h2><strong>Stay Connected</strong><span style="font-size: 17px; color: #494949;"> with Larry Kaifesh!</span></h3>
