3dd9534e50d0b19fbace15e39fb6fd25711aa197
doririvera <d-rivera@ignitorlabs.com>
2014-07-10 18:43:28 -0500

    slider speed

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -283,7 +283,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap requires jQuery'
   }
 
   Carousel.DEFAULTS = {
-    interval: 18000,
+    interval: 800,
     pause: 'hover',
     wrap: true
   }
