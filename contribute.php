3f9627989026f45fc043c56c1a680de6baa8981c
doririvera <d-rivera@ignitorlabs.com>
2014-07-09 19:38:16 -0500

    .

--- contribute.php
+++ contribute.php
@@ -61,25 +61,6 @@
 				
 				</div>
 				
-				<!--<div class="row">
-				
-				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="QRTMGGHF8QHSS">
-					<table>
-					<tr><td><input type="hidden" name="on0" value="Employer">Employer</td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
-					<tr><td><input type="hidden" name="on1" value="Occupation">Occupation</td></tr><tr><td><input type="text" name="os1" maxlength="200"></td></tr>
-					</table>
-					<input type="image" src="http://hstrial-kkaifesh1111.homestead.com/red-submit-button-hi1.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-					</form>
-					
-					
-					<hr>
-				
-				</div> -->
-				
-				
 				<div class="row">
 				
 				<h2>Please make checks payable to "Kaifesh for Congress"<br />
@@ -93,8 +74,6 @@
 				
 
 						</div>
-</div>
-
     </div>
     <!-- /.container -->
 
