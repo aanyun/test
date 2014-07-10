169a74a9418f54dc9ee1b7f072a368f1f30f9c4c
doririvera <d-rivera@ignitorlabs.com>
2014-07-09 19:05:09 -0500

    privacy policy

---
+++ privacy_policy.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kaifesh for Congress Official Website | Get Involved</title>
+
+    <!-- Bootstrap core CSS .. -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/modern-business.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700' rel='stylesheet' type='text/css'>
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	
+	
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="container">
+    	<?php include "view/header.php";?>
+	</div> <!-- / .container -->
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+			
+			   <h1 class="page-header"><strong>Get Involved</strong><br />
+                    <small>There are so many ways you can help support the Kaifesh for Congress campaign!</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a>
+                    </li>
+                    <li class="active">Get Involved</li>
+                </ol>
+			  
+            </div>
+			
+			</div>
+			
+			
+			            <div class="col-lg-12">
+
+	            <div class="row">
+
+<!-- START PRIVACY POLICY CODE --><div style="font-family:arial"><strong>What information do we collect?</strong> <br /><br />We collect information from you when you place an order, subscribe to our newsletter or fill out a form.  <br /><br />When ordering or registering on our site, as appropriate, you may be asked to enter your: name, e-mail address, mailing address, phone number or credit card information. You may, however, visit our site anonymously.<br /><br /><strong>What do we use your information for?</strong> <br /><br />Any of the information we collect from you may be used in one of the following ways: <br /><br />; To personalize your experience<br />(your information helps us to better respond to your individual needs)<br /><br />; To improve our website<br />(we continually strive to improve our website offerings based on the information and feedback we receive from you)<br /><br />; To improve customer service<br />(your information helps us to more effectively respond to your customer service requests and support needs)<br /><br />; To process transactions<br /><blockquote>Your information, whether public or private, will not be sold, exchanged, transferred, or given to any other company for any reason whatsoever, without your consent, other than for the express purpose of delivering the purchased product or service requested.</blockquote><br />; To send periodic emails<br /><blockquote>The email address you provide for order processing, may be used to send you information and updates pertaining to your order, in addition to receiving occasional company news, updates, related product or service information, etc.</blockquote>Note: If at any time you would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email.<br /><br /><br /><br /><strong>How do we protect your information?</strong> <br /><br />We implement a variety of security measures to maintain the safety of your personal information when you place an order or enter, submit, or access your personal information. <br /> <br />We offer the use of a secure server. All supplied sensitive/credit information is transmitted via Secure Socket Layer (SSL) technology and then encrypted into our Payment gateway providers database only to be accessible by those authorized with special access rights to such systems, and are required to?keep the information confidential.<br /><br />After a transaction, your private information (credit cards, social security numbers, financials, etc.) will not be stored on our servers.<br /><br /><strong>Do we use cookies?</strong> <br /><br />Yes (Cookies are small files that a site or its service provider transfers to your computers hard drive through your Web browser (if you allow) that enables the sites or service providers systems to recognize your browser and capture and remember certain information<br /><br /> We use cookies to compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future.<br /><br />If you prefer, you can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies via your browser settings. Like most websites, if you turn your cookies off, some of our services may not function properly. However, you can still place orders by contacting customer service.<br /><br /><strong>Do we disclose any information to outside parties?</strong> <br /><br />We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.<br /><br /><strong>Third party links</strong> <br /><br /> Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.<br /><br /><strong>California Online Privacy Protection Act Compliance</strong><br /><br />Because we value your privacy we have taken the necessary precautions to be in compliance with the California Online Privacy Protection Act. We therefore will not distribute your personal information to outside parties without your consent.<br /><br /><strong>Childrens Online Privacy Protection Act Compliance</strong> <br /><br />We are in compliance with the requirements of COPPA (Childrens Online Privacy Protection Act), we do not collect any information from anyone under 13 years of age. Our website, products and services are all directed to people who are at least 13 years old or older.<br /><br /><strong>Online Privacy Policy Only</strong> <br /><br />This online privacy policy applies only to information collected through our website and not to information collected offline.<br /><br /><strong>Your Consent</strong> <br /><br />By using our site, you consent to our <a style='text-decoration:none; color:#3C3C3C;' href='http://www.freeprivacypolicy.com/' target='_blank'>privacy policy</a>.<br /><br /><strong>Changes to our Privacy Policy</strong> <br /><br />If we decide to change our privacy policy, we will post those changes on this page. <br /><br /><strong>Contacting Us</strong> <br /><br />If there are any questions regarding this privacy policy you may contact us using the information below. <br /><br />http://www.kaifeshforcongress.org/<br />804 E Nerge Rd.<br />Roselle, IL 60172<br />United States<br />larry@kaifeshforcongress.com<br /><br /><span></span><span></span>This policy is powered by Trust Guard <a rel='nofollow' style='color:#000; text-decoration:none;' href='http://www.trust-guard.com/PCI-Compliance-s/65.htm' target='_blank'>PCI compliance</a> scans.<span></span><span></span><span></span><!-- END PRIVACY POLICY CODE -->
+					
+				</div>
+
+						</div>
+
+
+    </div>
+    <!-- /.container -->
+
+	
+	
+	
+	
+	    <div class="section">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-8 col-md-6 col-sm-6 subscribe">
+					<?php include "view/subscribeform.php";?>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 connect adjust2">
+					<h2><strong>Stay Connected</strong><span style="font-size: 17px; color: #494949;"> with Larry Kaifesh!</span></h3>
+					<hr class="divideConnect"> </hr>
+					
+					<a href="https://www.facebook.com/Kaifeshforcongress" target="_blank">
+						<img src="img/fb_icon.png" class="connect_icon">
+					</a>
+					<a href="https://twitter.com/LarryKaifesh" target="_blank">
+						<img src="img/twitter_icon.png" class="connect_icon">
+					</a>
+					<a href="https://www.youtube.com/user/kaifeshforcongress" target="_blank">
+						<img src="img/youtube_icon.png" class="connect_icon">
+					</a>
+					<a href="https://www.linkedin.com/in/kaifeshforcongress" target="_blank">
+						<img src="img/linkedin_icon.png" class="connect_icon">
+					</a>					
+					<a href="https://plus.google.com/110076519787570184443/posts" target="_blank">
+						<img src="img/gplus_icon.png" class="connect_icon">
+					</a>
+					<a href="http://www.pinterest.com/larrykaifesh/" target="_blank">
+						<img src="img/pin_icon.png" class="connect_icon">
+					</a>
+					<a href="mailto:larry@kaifeshforcongress.com">
+						<img src="img/email_icon.png" class="position: relative; top: -3px;">
+					</a>
+
+						<div style="margin-top: 15px;">
+						
+						
+		<div class="col-lg-6 col-md-6 col-sm-6" style="padding: 0;">
+		<a href="https://secure.jotform.us/larrykaifesh/kaifeshforcongress" target="_blank">
+  		<button class="btn btn-lg btn-primary btn-block" type="reset" style="width: 158px;">Contribute</button>
+		</a>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 space" style="padding: 0;">
+		<a href="get-involved.php" target="_blank">
+        <button class="btn btn-lg btn-danger btn-block" type="submit button" onclick="submit()" style="width: 158px;">Get Involved</button>
+		</a>
+		</div>
+						
+						
+
+						</div>
+						
+                </div> <!-- / .4 -->
+            </div> <!-- /.row -->
+           
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.section -->
+	
+	
+	
+        <footer>
+    <div class="section-colored">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-4 hide-1200minus">
+				<div class="paidforby">Paid for by Kaifesh for Congress</div>
+
+				<p class="subfooter ">804 E Nerge Rd.<br />
+					Roselle, IL 60172<br />
+					Office: 847-380-6915<br />
+					After hours: 224-800-8065</p>
+					
+
+                </div>
+				<div class="col-lg-4 col-md-12 hide-1200minus subfooter-center">
+                    <p>Copyright &copy; Kaifesh for Congress 2014.</p>
+					<img src="img/kaifesh_logo.png" class="img2" alt="Kaifesh for Congress">
+				</div>
+				<div class="col-lg-4 hide-1200minus subfooter-right">
+                    <p><a href="#">Privacy Policy</a> | <a href="contact-us.php">Contact</a></p>
+					</div>
+				
+				 <div class="col-md-4 col-md-push-4 hide-1200plus text-center">
+				 <span class="subfooter">Copyright &copy; Kaifesh for Congress 2014.</span><br />
+				 <img src="img/kaifesh_logo.png" class="img2" alt="Kaifesh for Congress" style="margin-top: 18px;">
+				</div>
+				
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+	
+    <!-- /.section-colored -->
+	    <div class="section-colored2 text-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="desclaimer">Larry Kaifesh is a Colonel in the United States Marine Corps Reserve.
+					<br />Use of his military rank, job titles, and photographs in uniform does not imply endorsement by the Marine Corps, Department of the Navy or the Department of Defense. </p>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.section-colored -->
+	</footer>
+	
+</div>
+<!-- /.wrapper -->
+
+
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/modern-business.js"></script>
+	<script src="js/jquery-1.9.1.js"></script>
+	<script src="js/main.js"></script>	
+
+</body>
+
+</html>
